c64039eb6fbab6fc768b04aab096ed9891c1a0d0
bassantahmed <bassantahmed2005xx@gmail.com>
2024-07-24 18:29:15 +0300

    task3

---
+++ lib/main.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+ runApp(Myapp());}
+ class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+   return MaterialApp(
+      title: 'task3',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Page1(),
+    );
+  }
+}
+
+class Page1 extends StatelessWidget {
+  const Page1({super.key});
+  
+  get margin => null;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+body:Container(
+  width: double.infinity,
+height: double.infinity,
+color: Colors.grey,
+
+
+child:Row(
+  mainAxisAlignment: MainAxisAlignment.center,
+  crossAxisAlignment: CrossAxisAlignment.center,
+  children: [ 
+Container(
+  width: 350,
+  height: 350,
+ color: Colors.cyan,
+ child: Column(
+  mainAxisAlignment: MainAxisAlignment.center,
+  crossAxisAlignment: CrossAxisAlignment.center,
+  children: [
+    const Text("The More You Practice..The less You Fail"),
+    Row(mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        ElevatedButton(onPressed: (){}, child: const Text("Agree")),
+         ElevatedButton(onPressed: (){}, child: const Text("do not agree")),
+
+      ],
+    )
+  ],)
+ 
+
+),
+const SizedBox(width: 200,),
+Container(width: 350,
+  height: 350,
+ color: Colors.brown,
+ child: Column
+ ( mainAxisAlignment: MainAxisAlignment.center,
+ crossAxisAlignment: CrossAxisAlignment.center,
+  children:[const Row(mainAxisAlignment: MainAxisAlignment.center,
+  crossAxisAlignment:CrossAxisAlignment.center,
+    children: [Text("code"),Icon(Icons.code)],
+  ),
+  const Row(mainAxisAlignment: MainAxisAlignment.center,
+  crossAxisAlignment:CrossAxisAlignment.center,
+    children: [Text("Eat"),Icon(Icons.restaurant)],
+  ),
+  const Row(mainAxisAlignment: MainAxisAlignment.center,
+  crossAxisAlignment:CrossAxisAlignment.center,
+    children: [Text("Sleep"),Icon(Icons.single_bed)],
+  ),
+  const Row(mainAxisAlignment: MainAxisAlignment.center,
+  crossAxisAlignment:CrossAxisAlignment.center,
+    children: [Text("Repeat"),Icon(Icons.repeat)],
+  ),
+  Row(mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        ElevatedButton(onPressed: (){}, child: const Text("Agree")),
+         ElevatedButton(onPressed: (){}, child: const Text("do not agree")),
+
+      ],
+    ),
+ 
+
+  ],),
+
+
+
+),
+const SizedBox(width: 200,),
+Container(
+  height: 350,
+  width: 350,
+  color: Colors.deepPurpleAccent,
+  child:Column(mainAxisAlignment: MainAxisAlignment.center
+  ,crossAxisAlignment: CrossAxisAlignment.center,
+    children: [const Text("The More You Fail..The More You Learn"),
+    Row(mainAxisAlignment: MainAxisAlignment.spaceAround,
+    
+      children: [
+        ElevatedButton(onPressed: (){}, child: const Text("Agree")),
+         ElevatedButton(onPressed: (){}, child: const Text("do not agree")),
+
+      ],
+    ),
+
+    
+
+  ],),
+)
+  ],)
+     ) );
+
+  
+    
+    
+
+    
+    
+
+}
+}
+ 
